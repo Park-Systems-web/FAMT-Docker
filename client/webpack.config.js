9579211754e25b74abca33c51cf85fd379f5e5b2
rnintai <ho46825@gmail.com>
2022-05-30 09:29:48 +0900

    cors test

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -77,7 +77,9 @@ module.exports = (env, argv) => {
     devServer: {
       historyApiFallback: true,
       proxy: {
-        "/api": "http://localhost:5000",
+        // "/api": "http://localhost:5000",
+        "/api":
+          "http://ec2-18-144-161-137.us-west-1.compute.amazonaws.com:5001",
       },
     },
     // uglify
--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ app.use(
   swaggerUi.setup(require("./config/swaggerDoc"))
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log("Sever is running on port ", PORT);
 });
