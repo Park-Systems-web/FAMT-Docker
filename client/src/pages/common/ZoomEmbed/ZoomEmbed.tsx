22feb713784578c584c900603d6bb887b1463a39
rnintai <ho46825@gmail.com>
2022-07-13 18:29:06 +0900

    Revert "Fix: embed test"
    This reverts commit eccbc8c544819267577dedc31a0d7ba1e0947b3c.

--- client/src/pages/common/ZoomEmbed/ZoomEmbed.tsx
+++ client/src/pages/common/ZoomEmbed/ZoomEmbed.tsx
@@ -1,24 +1,14 @@
 import React, { useEffect, useState } from "react";
 // import { ZoomMtg } from "@zoomus/websdk";
 import ZoomMtgEmbedded from "@zoomus/websdk/embedded";
-import { ZoomMtg } from "@zoomus/websdk";
-
 import { useAuthState } from "context/AuthContext";
 import { useNavigate, useParams } from "react-router";
 import axios from "axios";
 import { useSearchParams } from "react-router-dom";
+
 // embed
 
 const ZoomEmbed = () => {
-  ZoomMtg.preLoadWasm();
-  ZoomMtg.prepareJssdk();
-
-  // loads language files, also passes any error messages to the ui
-  ZoomMtg.i18n.load("en-US");
-  ZoomMtg.i18n.reload("en-US");
-
-  ZoomMtg.setZoomJSLib("https://source.zoom.us/{VERSION_NUMBER}/lib", "/av");
-
   const [currentZoomSignature, setCurrentZoomSignature] = useState<string>("");
   const authState = useAuthState();
   const { webinarId } = useParams();
@@ -50,102 +40,63 @@ const ZoomEmbed = () => {
     };
   }, []);
 
-  // useEffect(() => {
-  //   const client = ZoomMtgEmbedded.createClient();
-  //   const meetingSDKElement = document.getElementById("meetingSDKElement");
-  //   const meetingSDKChatElement = document.getElementById(
-  //     "meetingSDKChatElement",
-  //   );
-
-  //   try {
-  //     client.init({
-  //       // debug: true,
-  //       zoomAppRoot: meetingSDKElement,
-  //       language: "en-US",
-  //       customize: {
-  //         meetingInfo: [
-  //           "topic",
-  //           "host",
-  //           "mn",
-  //           "pwd",
-  //           "telPwd",
-  //           "participant",
-  //           "dc",
-  //           "enctype",
-  //         ],
-  //         video: {
-  //           popper: {
-  //             disableDraggable: true,
-  //           },
-  //           viewSizes: {
-  //             default: {
-  //               width: isMobile ? 350 : 1385,
-  //               height: 660,
-  //             },
-  //           },
-  //         },
-  //         chat: {
-  //           popper: {
-  //             anchorElement: meetingSDKChatElement,
-  //           },
-  //         },
-  //       },
-  //     });
-
-  //     client.join({
-  //       sdkKey: process.env.ZOOM_SDK_KEY,
-  //       signature: currentZoomSignature,
-  //       meetingNumber: webinarId,
-  //       password: "",
-  //       userName: authState.name,
-  //       userEmail: authState.email,
-  //       tk: searchParams.get("tk"),
-  //     });
-  //   } catch (error) {
-  //     console.log(error.reason);
-  //   }
-
-  //   return () => {
-  //     client.leaveMeeting();
-  //   };
-  // }, [currentZoomSignature]);
   useEffect(() => {
-    document.getElementById("zmmtg-root").style.display = "block";
+    const client = ZoomMtgEmbedded.createClient();
+    const meetingSDKElement = document.getElementById("meetingSDKElement");
+    const meetingSDKChatElement = document.getElementById(
+      "meetingSDKChatElement",
+    );
 
-    ZoomMtg.init({
-      leaveUrl: "https://famt.parksystems.com/lecture-hall",
-      success: (success) => {
-        console.log(success);
-
-        ZoomMtg.join({
-          signature: currentZoomSignature,
-          meetingNumber: webinarId,
-          userName: authState.name,
-          sdkKey: process.env.ZOOM_SDK_KEY,
-          userEmail: authState.email,
-          passWord: "",
-          tk: searchParams.get("tk"),
-          success: (success) => {
-            console.log(success);
+    try {
+      client.init({
+        // debug: true,
+        zoomAppRoot: meetingSDKElement,
+        language: "en-US",
+        customize: {
+          meetingInfo: [
+            "topic",
+            "host",
+            "mn",
+            "pwd",
+            "telPwd",
+            "participant",
+            "dc",
+            "enctype",
+          ],
+          video: {
+            popper: {
+              disableDraggable: true,
+            },
+            viewSizes: {
+              default: {
+                width: 1385,
+                height: 660,
+              },
+            },
           },
-          error: (error) => {
-            console.log(error);
+          chat: {
+            popper: {
+              anchorElement: meetingSDKChatElement,
+            },
           },
-        });
-      },
-      error: (error) => {
-        console.log(error);
-      },
-    });
-
-    return () => {
-      document.getElementById("zmmtg-root").style.display = "none";
-
-      ZoomMtg.leaveMeeting({
-        success: () => {
-          //
         },
       });
+
+      client.join({
+        sdkKey: process.env.ZOOM_SDK_KEY,
+        signature: currentZoomSignature,
+        meetingNumber: webinarId,
+        password: "",
+        userName: authState.name,
+        userEmail: authState.email,
+        tk: searchParams.get("tk"),
+      });
+    } catch (error) {
+      console.log(error.reason);
+    }
+
+    return () => {
+      client.leaveMeeting();
     };
   }, [currentZoomSignature]);
 
